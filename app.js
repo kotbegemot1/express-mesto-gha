58bfe3e86aefb38f0f3613bd4209b22c2cbe5fe7
kotbegemot1 <kk1slorod@gmail.com>
2023-07-19 16:28:47 +0000

    init commit from server

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
 });
 
 // eslint-disable-next-line no-undef
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
